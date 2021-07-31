436b235f8c3c205b45fbb27ae7e246f1240bac67
Robert Schwartz <bschwartz23@gmail.com>
2021-07-31 15:46:12 -0700

    set up mutations.js in client utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,56 @@
+// import GraphQL
+import { gql } from "@apollo/client";
+
+// Login
+export const LOGIN_USER = gql`
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+// Add User
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+// Save book
+export const SAVE_BOOK = gql`
+	mutation saveBook($content: BookInfo!) {
+		saveBook(content: $content) {
+			_id
+			username
+			savedBooks {
+				bookId
+				title
+			}
+		}
+	}
+`;
+
+// Remove Book
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId: String!) {
+		removeBook(bookId: $bookId) {
+			_id
+			username
+			savedBooks {
+				bookId
+				title
+			}
+		}
+	}
+`;
