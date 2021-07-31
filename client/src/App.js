7db8021ca8d81918a2af2eaadb2b7a62352ac2c9
Robert Schwartz <bschwartz23@gmail.com>
2021-07-31 15:12:11 -0700

    set up Apollo provider and set links in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,64 @@
+// React
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// ==============================================
+
+// import Apollo
+import {
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+	createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+// ==============================================
+
+// import pages and components
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
+// ==============================================
+
+// Link to graphQL server
+const httpLink = createHttpLink({
+	uri: "/graphql",
+});
+// ==============================================
+
+// set headers to the user Auth token for each request
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+// link to API 
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+// ==============================================
 
 function App() {
 	return (
-		<Router>
-			<>
-				<Navbar />
-				<Switch>
-					<Route exact path="/" component={SearchBooks} />
-					<Route exact path="/saved" component={SavedBooks} />
-					<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-				</Switch>
-			</>
-		</Router>
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={SearchBooks} />
+						<Route exact path="/saved" component={SavedBooks} />
+						<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
 	);
 }
+// ==============================================
 
 export default App;
