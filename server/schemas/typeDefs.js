afdbdb3ede4cf12509a917228514890b27654b16
Robert Schwartz <bschwartz23@gmail.com>
2021-07-31 13:29:08 -0700

    add Book Type to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,21 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String
-        bookCount: INT
-        savedBooks: [Book]
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: INT
+    savedBooks: [Book]
+  }
 
-    type Book {
-
-    }
-`
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+`;
 module.exports = typeDefs;
